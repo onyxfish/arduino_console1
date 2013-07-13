39a19a85720f38b5afadf539bca42ed23eae3f16
Christopher Groskopf <staringmonkey@gmail.com>
2013-07-13 13:05:24 -0500

    Support right LEDs.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,6 +1,16 @@
 #include <avr/sleep.h>
 #include <SPI.h>
 
+/*
+    Driver A = Upper left = leftState[0]
+    Driver B = leftState[1]
+    Driver C = leftState[2]
+    Driver D = Lower left = leftState[3]
+
+    Driver E = Upper Right = rightState[0]
+    Driver F = Lower Right rightState[1]
+*/
+
 const byte LEFT_DRIVERS = 4;
 const byte RIGHT_DRIVERS = 2;
 const byte LEFT_LED_PAIRS = 16;
@@ -9,11 +19,15 @@ const byte RIGHT_LED_PAIRS = 7;
 //int ledClockPin = 13;
 //int ledDataPin = 11;
 byte ledLatchPin = 10;
-byte switchPin = 2;
-byte leftPin = A1;
+byte leftSwitchPin = 2;
+byte rightSwitchPin = 3;
+byte leftPotPin = A1;
+byte rightPotPin = A2;
 
-byte leftLit = 0;
-byte rightLit = 0;
+bool leftOn = false;
+bool rightOn = false;
+byte leftNumLit = 0;
+byte rightNumLit = 0;
 byte leftState[LEFT_DRIVERS];
 byte rightState[RIGHT_DRIVERS];
 
@@ -21,65 +35,79 @@ byte rightState[RIGHT_DRIVERS];
  * Initialize the app.
  */
 void setup() { 
-    Serial.begin(9600);
+    //Serial.begin(9600);
 
     pinMode(ledLatchPin, OUTPUT);
-    pinMode(switchPin, INPUT_PULLUP);
+    pinMode(leftSwitchPin, INPUT_PULLUP);
+    pinMode(rightSwitchPin, INPUT_PULLUP);
   
     SPI.begin();
     SPI.setBitOrder(LSBFIRST);
     SPI.setClockDivider(SPI_CLOCK_DIV2);
 
-    leftLit = (float(analogRead(leftPin)) / 1000) * 16;
+    leftOn = (digitalRead(leftSwitchPin) == LOW);
+    rightOn = (digitalRead(rightSwitchPin) == LOW);
+    leftNumLit = (float(analogRead(leftPotPin)) / 1000) * 16;
+    rightNumLit = (float(analogRead(rightPotPin)) / 1000) * 16;
 
     // Setup initial state
-    updateState();
+    updateLeftState();
+    updateRightState();
     updateLeds();
 } 
 
-void updateState() {
-    if (leftLit == 16) {
+void updateLeftState() {
+    if (!leftOn) {
+        leftState[0] = 0;
+        leftState[1] = 0;
+        leftState[2] = 0;
+        leftState[3] = 0;
+
+        return;
+    }
+
+    if (leftNumLit == 16) {
         leftState[0] = 0b10101010;
         leftState[1] = 0b10101010;
         leftState[2] = 0b10101010;
         leftState[3] = 0b10101010;
-    } else if (leftLit >= 12) {
+    } else if (leftNumLit >= 12) {
         leftState[0] = 0b10101010;
         leftState[1] = 0b10101010;
         leftState[2] = 0b10101010;
 
-        if (leftLit == 12) {
+        if (leftNumLit == 12) {
             leftState[3] = 0b01010101;
-        } else if (leftLit == 13) {
+        } else if (leftNumLit == 13) {
             leftState[3] = 0b10010101;
-        } else if (leftLit == 14) {
+        } else if (leftNumLit == 14) {
             leftState[3] = 0b10100101;
         } else {
             leftState[3] = 0b10101001;
         }
-    } else if (leftLit >= 8) {
+    } else if (leftNumLit >= 8) {
         leftState[0] = 0b10101010;
         leftState[1] = 0b10101010;
 
-        if (leftLit == 8) {
+        if (leftNumLit == 8) {
             leftState[2] = 0b01010101;
-        } else if (leftLit == 9) {
+        } else if (leftNumLit == 9) {
             leftState[2] = 0b10010101;
-        } else if (leftLit == 10) {
+        } else if (leftNumLit == 10) {
             leftState[2] = 0b10100101;
         } else {
             leftState[2] = 0b10101001;
         }
 
         leftState[3] = 0b01010101;
-    } else if (leftLit >= 4) {
+    } else if (leftNumLit >= 4) {
         leftState[0] = 0b10101010;
         
-        if (leftLit == 4) {
+        if (leftNumLit == 4) {
             leftState[1] = 0b01010101;
-        } else if (leftLit == 5) {
+        } else if (leftNumLit == 5) {
             leftState[1] = 0b10010101;
-        } else if (leftLit == 6) {
+        } else if (leftNumLit == 6) {
             leftState[1] = 0b10100101;
         } else {
             leftState[1] = 0b10101001;
@@ -88,11 +116,11 @@ void updateState() {
         leftState[2] = 0b01010101;
         leftState[3] = 0b01010101;
     } else {
-        if (leftLit == 0) {
+        if (leftNumLit == 0) {
             leftState[0] = 0b01010101;
-        } else if (leftLit == 1) {
+        } else if (leftNumLit == 1) {
             leftState[0] = 0b10010101;
-        } else if (leftLit == 2) {
+        } else if (leftNumLit == 2) {
             leftState[0] = 0b10100101;
         } else {
             leftState[0] = 0b10101001;
@@ -104,18 +132,64 @@ void updateState() {
     }
 }
 
+void updateRightState() {
+    if (!rightOn) {
+        rightState[0] = 0;
+        rightState[1] = 0;
+
+        return;
+    }
+
+    if (rightNumLit >= 8) {
+        rightState[0] = 0b10101010;
+        rightState[1] = 0b10101010;
+    } else if (rightNumLit >= 4) {
+        rightState[0] = 0b10101010;
+        
+        if (rightNumLit == 4) {
+            rightState[1] = 0b01010101;
+        } else if (rightNumLit == 5) {
+            rightState[1] = 0b10010101;
+        } else if (rightNumLit == 6) {
+            rightState[1] = 0b10100101;
+        } else {
+            rightState[1] = 0b10101001;
+        }
+    } else {
+        if (rightNumLit == 0) {
+            rightState[0] = 0b01010101;
+        } else if (rightNumLit == 1) {
+            rightState[0] = 0b10010101;
+        } else if (rightNumLit == 2) {
+            rightState[0] = 0b10100101;
+        } else {
+            rightState[0] = 0b10101001;
+        }
+
+        rightState[1] = 0b01010101;
+    }
+}
+
+
 /*
  * Main loop. Do nothing since interrupts are running.
  */
 void loop() { 
-    byte newLeftLit = (float(analogRead(leftPin)) / 1000) * 16;
+    bool newLeftOn = (digitalRead(leftSwitchPin) == LOW);
+    bool newRightOn = (digitalRead(rightSwitchPin) == LOW);
+    byte newLeftNumLit = (float(analogRead(leftPotPin)) / 1000) * 16;
+    byte newRightNumLit = (float(analogRead(rightPotPin)) / 1000) * 16;
 
-    Serial.println(newLeftLit);
+    //Serial.println(newLeftLit);
 
-    if (newLeftLit != leftLit) {
-        leftLit = newLeftLit;
+    if (newRightOn != rightOn || newRightNumLit  != rightNumLit || newLeftOn != leftOn|| newLeftNumLit != leftNumLit) {
+        leftOn = newLeftOn;
+        rightOn = newRightOn;
+        leftNumLit = newLeftNumLit;
+        rightNumLit = newRightNumLit;
 
-        updateState();
+        updateLeftState();
+        updateRightState();
         updateLeds();
     }
 }
@@ -126,13 +200,12 @@ void loop() {
 void updateLeds() {
     digitalWrite(ledLatchPin, LOW);
 
-    for (byte i = 0; i < LEFT_DRIVERS; i++) {
-        SPI.transfer(leftState[i]);
-    }
-
-    for (byte i = 0; i < RIGHT_DRIVERS; i++) {
-        SPI.transfer(rightState[i]);
-    }
+    SPI.transfer(rightState[0]);
+    SPI.transfer(rightState[1]);
+    SPI.transfer(leftState[0]);
+    SPI.transfer(leftState[1]);
+    SPI.transfer(leftState[2]);
+    SPI.transfer(leftState[3]);
 
     digitalWrite(ledLatchPin, HIGH);
 }
